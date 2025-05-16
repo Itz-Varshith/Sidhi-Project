5559c99bc22261c9efc83618554f91d4a3574368
S Varshith Reddy <svarshithreddy9121@gmail.com>
2025-05-16 16:40:01 +0530

    Added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      This section holds the navbar for the pages.
-      may contain sections like home, contact us, etc. based on the requirement. 
-    </div>
-  )
-}
+    <nav className="flex justify-between items-center top-0 left-0 px-3 py-3 m-0 w-full bg-[#E5ECE9]">
+      <div>
+        <div className=" flex justify-between ">
+          <a href="https://www.iiti.ac.in">
+            <img
+              src="/IITI Logo.png"
+              className=" px-2 py-2 m-2 h-15"
+              alt="IITI logo"
+            />
+          </a>
+          <a href="https://charakcenter.iiti.ac.in/">
+            <img
+              src="/charak.png"
+              className=" px-2 py-2 m-2 h-15"
+              alt="Charak logo"
+            />
+          </a>
+          <a href="https://drishticps.iiti.ac.in/">
+            <img
+              src="/Drishti.png"
+              className=" px-2 py-2 m-2 h-15"
+              alt="Drishti logo"
+            />
+          </a>
+        </div>
+        <h1 className="text-center"> CHARAK DIGITAL TWIN PLATFORM</h1>
+      </div>
+      <div className="w-[80%] p-3 text-center text-3xl font-extrabold">
+        <h1 >DATA COLLECTION INTERFACE</h1>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
