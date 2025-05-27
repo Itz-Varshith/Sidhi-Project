15267489eef12b65117e3c1fc2ea4aedeff5b1f7
S Varshith Reddy <svarshithreddy9121@gmail.com>
2025-05-27 11:34:19 +0530

    Minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import Ventilator from '../components/Ventilator.jsx';
 
 const Home = () => {
   //We will use a component based system so that we can easily manage the data fetching part of the system.
+  //The styling for the layut will be added here.
+  //The components will only contain the code for the data of the container.
   return (
     <div>
       <div>
